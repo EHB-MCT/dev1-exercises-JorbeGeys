41f4a7ed758ed4f2e621f6c8e9fc1a8e439690e8
Jorbe Geys <jorbe.geys@student.ehb.be>
2022-10-05 12:04:32 +0200

    final

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,6 +1,78 @@
 "use strict";
 
-Context.beginpath();
-Context.fill("black");
-Context.rect(100,100,5000,2000);
-context.stroke();
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawName();
+
+function drawName (){
+    context.lineWidth = 5;
+    context.strokeStyle = "red";
+    
+    context.beginPath();
+
+    context.moveTo(300,300);
+    context.lineTo(300,500);
+    context.lineTo(250,525);
+    context.lineTo(200,500);
+    
+    context.stroke();
+
+
+
+    context.strokeStyle = "yellow";
+    context.beginPath();
+
+    context.moveTo(400,300);
+    context.lineTo(400,500);
+    context.lineTo(500,500);
+    context.lineTo(500,300);
+    context.lineTo(400,300);
+
+    context.stroke();
+
+
+
+    context.strokeStyle = "blue";
+    context.beginPath();
+
+    context.moveTo(600,500);
+    context.lineTo(600,300);
+    context.lineTo(700,300);
+    context.lineTo(700,400);
+    context.lineTo(600,400);
+    context.lineTo(700,500);
+
+    context.stroke();
+
+
+
+    context.strokeStyle = "green";
+    context.beginPath();
+
+    context.moveTo(800,400);
+    context.lineTo(800,300);
+    context.lineTo(900,300);
+    context.lineTo(900,500);
+    context.lineTo(800,500);
+    context.lineTo(800,400);
+    context.lineTo(900,400);
+
+    context.stroke();
+
+
+
+    context.strokeStyle = "purple";
+    context.beginPath();
+
+    context.moveTo(1100,300);
+    context.lineTo(1000,300);
+    context.lineTo(1000,500);
+    context.lineTo(1100,500);
+    context.moveTo(1000,400);
+    context.lineTo(1100,400);
+
+    context.stroke();
+}
