6f39b1e88cfdb7134e2aec015ad1e22d36482e17
Jorbe Geys <jorbe.geys@student.ehb.be>
2022-10-12 10:34:04 +0200

    2-1-moreLines

--- exercises/2-variables/2-1-moreLines.js
+++ exercises/2-variables/2-1-moreLines.js
@@ -12,7 +12,7 @@ let height = context.canvas.height;
 console.log("height", height);
 
 let marginW = 50;
-let marginH = 50;
+let marginH = 200;
 console.log("height_without_margins" , height-2*marginH);
 
 drawlines();
@@ -28,32 +28,32 @@ function drawlines(){
     context.lineTo(width-marginW , marginH);
 
     //1ste schuine lijn
-    context.lineTo(marginW , (height-(2*marginH))/4);
-    console.log("einde_schuine_lijn" , (height-(2*marginH))/4);
+    context.lineTo(marginW , marginH +(height-(2*marginH))/4);
+    console.log("einde_schuine_lijn" , marginH + (height-(2*marginH))/4);
 
     //2de horizontale lijn
-    context.lineTo(width-marginW , (height-(2*marginH))/4);
+    context.lineTo(width-marginW , marginH + (height-(2*marginH))/4);
 
     //2de schuine lijn
-    context.lineTo(marginW , (height-(2*marginH))/2);
-    console.log("einde_schuine_lijn" , (height-(2*marginH))/2);
+    context.lineTo(marginW , marginH + (height-(2*marginH))/2);
+    console.log("einde_schuine_lijn" , marginH + (height-(2*marginH))/2);
 
     //3de horizontale lijn
-    context.lineTo(width-marginW , (height-(2*marginH))/2);
+    context.lineTo(width-marginW , marginH + (height-(2*marginH))/2);
 
     //3de schuine lijn
-    context.lineTo(marginW , (height-(2*marginH))*3/4);
-    console.log("einde_schuine_lijn" , (height-(2*marginH))*3/4);
+    context.lineTo(marginW , marginH + (height-(2*marginH))*3/4);
+    console.log("einde_schuine_lijn" , marginH + (height-(2*marginH))*3/4);
 
     //4de horizontale lijn
-    context.lineTo(width-marginW , (height-(2*marginH))*3/4);
+    context.lineTo(width-marginW , marginH + (height-(2*marginH))*3/4);
 
     //4de schuine lijn
-    context.lineTo(marginW , height-(2*marginH));
-    console.log("einde_schuine_lijn" , height-(2*marginH));
+    context.lineTo(marginW , height-marginH);
+    console.log("einde_schuine_lijn" , height-marginH);
 
     //5de horizontale lijn
-    context.lineTo(width-marginW , height-(2*marginH));
+    context.lineTo(width-marginW , height-marginH);
 
     //grote schuine lijn
     context.lineTo(marginW , marginH);
