0bac39d0fd0ce644f7aedf1795c5c2daf5b8ad57
Jorbe Geys <jorbe.geys@student.ehb.be>
2022-10-12 12:31:57 +0200

    2-3-pattern

--- exercises/2-variables/2-3-pattern.js
+++ exercises/2-variables/2-3-pattern.js
@@ -18,16 +18,16 @@ pattern();
 
 
 function pattern (){
-    
+    //donkerblauwe achtergrond
     context.fillStyle = "#00008b";
     context.fillRect(marginW   ,   marginH   ,   width-2*marginW   ,   (height-2*marginH)*2/3);
-
+    //lichtblauwe onderste balk
     context.fillStyle = "#add8e6";
     context.fillRect(marginW   ,   marginH + (height-2*marginH)*2/3 ,   width-2*marginW   ,   (height-2*marginH)/3);
-
+    //beige kollom
     context.fillStyle = "#a48f7a";
     context.fillRect(marginW + (width-2*marginW)/3   ,   marginH   ,   (width-2*marginW)/3   ,   height - 2*marginH);
-    
+    //rood vak
     context.fillStyle = "rgb(255,0,0)";
     context.fillRect(marginW   ,   marginH + (1/4)*((height-2*marginH)*2/3) ,   width-2*marginW   ,   (height-2*marginH)/3);
 
